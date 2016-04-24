6ae9b0b7e02c4be66d20ef6a774f54bbe93fa5f8
tgb <timmey.g.b@gmail.com>
2016-04-23 23:21:07 -0700

    Stubbed out watchdog.

---
+++ Watchdog/AbstractWatchdog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watchdog
+{
+    abstract class AbstractWatchdog
+    {
+        public abstract AbstractWatchdog GetInstance();
+        public abstract void Watch();
+        protected abstract void ConsumeNewMessages(object state);
+        
+    }
+}
