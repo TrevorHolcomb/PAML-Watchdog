6e660b16024fa25eec92bf30a208c774abb7f24b
John Rowley <john@johnrowley.co>
2016-04-24 17:11:53 -0700

    Rename project to be more descriptive and remove the producer inside of the WatchdogDaemon (unneeded)
    Any producer related content should stay in the message generator project.

--- WatchdogDaemon/PollingWatchdog.cs
+++ WatchdogDaemon/PollingWatchdog.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading;
 using WatchdogDatabaseAccessLayer;
 
-namespace Watchdog
+namespace WatchdogDaemon
 {
     class PollingWatchdog : AbstractWatchdog
     {
---
+++ WatchdogDaemon/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Data.Entity;
+using WatchdogDatabaseAccessLayer;
+using WatchdogMessageGenerator;
+using System.Threading;
+
+namespace WatchdogDaemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Watchdog simulator started");
+            //start consumer
+            AbstractWatchdog rex = new PollingWatchdog().GetInstance();
+            rex.Watch();
+
+            while (true) { }
+        }
+    }
+}
