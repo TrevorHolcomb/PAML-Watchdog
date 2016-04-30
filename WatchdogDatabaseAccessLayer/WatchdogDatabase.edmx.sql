b78cab3fb6dee98ca6ba453a0bf8391a0056293f
John Rowley <john@johnrowley.co>
2016-04-30 02:19:10 -0700

    Added Help page and documentation on setting up the database.

--- WatchdogDatabaseAccessLayer/WatchdogDatabase.edmx.sql
+++ WatchdogDatabaseAccessLayer/WatchdogDatabase.edmx.sql
@@ -2,13 +2,13 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 04/28/2016 22:42:56
--- Generated from EDMX file: E:\Users\tgb\Documents\Visual Studio 2015\Projects\paml-watchdog\WatchdogDatabaseAccessLayer\WatchdogDatabase.edmx
+-- Date Created: 04/30/2016 01:16:21
+-- Generated from EDMX file: C:\Users\RowleyJohn\Documents\Visual Studio 2015\Projects\paml-watchdog\WatchdogDatabaseAccessLayer\WatchdogDatabase.edmx
 -- --------------------------------------------------
 
 SET QUOTED_IDENTIFIER OFF;
 GO
-USE [watchdog2];
+USE [watchdog];
 GO
 IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
 GO
