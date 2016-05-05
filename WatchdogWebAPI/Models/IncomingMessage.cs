b22f0a793cbf0e90da681e7b322bcd307f2f5abf
Tanner Cook <tannerncook@yahoo.com>
2016-05-04 20:07:49 -0700

    Basic functionality of WebAPI complete, can post single messages, get single message types by id, or all message types

---
+++ WatchdogWebAPI/Models/IncomingMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WatchdogWebAPI.Models
+{
+    public class IncomingMessage
+    {
+        public string Server { get; set; }
+        public int MessageTypeId { get; set; }
+        public object Params { get; set; }
+    }
+
+    public class OutGoingMessage
+    {
+        public string Server { get; set; }
+        public string Origin { get; set; }
+        public int MessageTypeId { get; set; }
+        public object Params { get; set; }
+    }
+}
