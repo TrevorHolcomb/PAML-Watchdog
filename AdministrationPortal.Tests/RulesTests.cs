28c70eed61728bc414a976f409e457c6a5e86ce7
John Rowley <johnrowleyster@gmail.com>
2016-08-12 08:59:24 -0700

    investigating selenium

---
+++ AdministrationPortal.Tests/RulesTests.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using Xunit;
+
+namespace AdministrationPortal.Tests
+{
+    public class RulesTests
+    {
+        [Fact]
+        public void CreateRuleTest()
+        {
+            IWebDriver driver = new InternetExplorerDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+        }
+    }
+}
