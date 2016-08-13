e807e7acb88cfe136ee9d226dbcb37d321dee1dc
John Rowley <johnrowleyster@gmail.com>
2016-08-13 09:42:38 -0700

    Completed regression test for rule creation.

--- AdministrationPortal.Tests/RulesTests.cs
+++ AdministrationPortal.Tests/RulesTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using Coypu;
 using Coypu.Drivers;
 using Coypu.Drivers.Selenium;
@@ -22,9 +23,41 @@ namespace AdministrationPortal.Tests
         private const string CreatedBy = "Selenium & Coypu";
         private const string Expression = "";
         private const string DefaultSeverity = "2";
-        private const string AlertType = "Standard Alert Type";
-        private const string MessageType = "QueueSize";
+        private const string AlertTypeName = "Standard Alert Type";
+        private const string AlertTypeDescription = "the standard alert type";
+        private const string MessageTypeName = "QueueSize";
+
+        private const string MessageTypeDescription =
+            "A simple update from a queue with the number of elements enqueued in it";
+
         private const string SupportCategory = "Standard Support Group";
+        private const string SupportCategoryDescription = "The standard support group";
+        private const string RuleCategoryName = "N/A";
+
+        private const string RuleCategoryDescription =
+            "This is a rule category for rules that don't mach into a single group";
+
+        private void BuildMessageType(WatchdogDatabaseContainer db)
+        {
+            var messageType = new MessageType
+            {
+                Name = RulesTests.MessageTypeName,
+                Description = MessageTypeDescription
+            };
+
+            var messageTypeParameterType = new MessageTypeParameterType
+            {
+                Name = "QueueSize",
+                MessageType = messageType,
+                Required = true,
+                Type = "integer"
+            };
+
+            messageType.MessageTypeParameterTypes.Add(messageTypeParameterType);
+
+            db.MessageTypes.Add(messageType);
+            db.MessageTypeParameterTypes.Add(messageTypeParameterType);
+        }
 
         [Fact]
         public void CreateRuleTest()
@@ -40,7 +73,13 @@ namespace AdministrationPortal.Tests
             }))
             {
                 db.DeleteAll();
-                db.SupportCategories.Add(new SupportCategory {Name = SupportCategory});
+                db.SupportCategories.Add(new SupportCategory {Name = SupportCategory, Description = SupportCategoryDescription});
+                db.Engines.Add(new Engine {Name = RuleEngine});
+                db.AlertTypes.Add(new AlertType {Name = AlertTypeName, Description =  AlertTypeDescription});
+                db.RuleCategories.Add(new RuleCategory {Name = RuleCategoryName, Description = RuleCategoryDescription});
+                BuildMessageType(db);
+
+
                 db.SaveChanges();
 
                 browser.Visit("/Rules");
@@ -53,13 +92,22 @@ namespace AdministrationPortal.Tests
                 browser.FillIn("Origin").With(Origin);
                 browser.FillIn("Server").With(Server);
                 browser.FillIn("RuleCreator").With(CreatedBy);
-                browser.FillIn("Expression").With(Expression);
+                
+                // Build Expression
+                browser.Select("QueueSize").From("builder_rule_0_filter");
+                browser.Select("greater").From("builder_rule_0_operator");
+                browser.FillIn("builder_rule_0_value_0").With("10");
+
                 browser.Select(DefaultSeverity).From("DefaultSeverity");
-                browser.Select(AlertType).From("AlertTypeId");
-                browser.Select(MessageType).From("MessageTypeName");
+                browser.Select(AlertTypeName).From("AlertTypeId");
+                browser.Select(MessageTypeName).From("MessageTypeName");
                 browser.Select(SupportCategory).From("SupportCategoryId");
 
+                // Select Rule Category
+                browser.ExecuteScript($@"$('#SelectedRuleCategoryIds').multiselect('select', '{db.RuleCategories.Single(e => e.Name == RuleCategoryName).Id}')");
+                
                 browser.ClickButton("Create");
+                Assert.Equal(1, db.Rules.Count());
             }
         }
     }
--- WatchdogDatabaseAccessLayer/Models/WatchdogDatabaseContainerPartial.cs
+++ WatchdogDatabaseAccessLayer/Models/WatchdogDatabaseContainerPartial.cs
@@ -8,33 +8,59 @@ namespace WatchdogDatabaseAccessLayer.Models
 {
     public partial class WatchdogDatabaseContainer
     {
+        private void ClearDatabase()
+        {
+            // disable all foreign keys
+            //context.Database.ExecuteSqlCommand("EXEC sp_MSforeachtable @command1 = 'ALTER TABLE ? NOCHECK CONSTRAINT all'");
+
+            List<string> tableNames = Database.SqlQuery<string>("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_NAME NOT LIKE '%Migration%'").ToList();
+
+            for (int i = 0; tableNames.Count > 0; i++)
+            {
+                try
+                {
+                    Database.ExecuteSqlCommand($"DELETE FROM {tableNames.ElementAt(i%tableNames.Count)}");
+                    tableNames.RemoveAt(i % tableNames.Count);
+                    i = 0;
+                }
+                catch { } // ignore errors as these are expected due to linked foreign key data             
+            }
+
+
+            SaveChanges();
+        }
+
         public void DeleteAll()
         {
-            Messages.ToList().RemoveAll(e => true);
-            MessageTypes.ToList().RemoveAll(e => true);
-            EscalationChains.ToList().RemoveAll(e => true);
-            EscalationChainLinks.ToList().RemoveAll(e => true);
-            Notifyees.ToList().RemoveAll(e => true);
-            NotifyeeGroups.ToList().RemoveAll(e => true);
-            Rules.ToList().RemoveAll(e => true);
-            AlertTypes.ToList().RemoveAll(e => true);
-            Alerts.ToList().RemoveAll(e => true);
-            RuleCategories.ToList().RemoveAll(e => true);
-            MessageTypeParameterTypes.ToList().RemoveAll(e => true);
-            MessageParameters.ToList().RemoveAll(e => true);
-            AlertParameters.ToList().RemoveAll(e => true);
-            SupportCategories.ToList().RemoveAll(e => true);
-            Engines.ToList().RemoveAll(e => true);
-            AlertStatuses.ToList().RemoveAll(e => true);
-            UnvalidatedMessages.ToList().RemoveAll(e => true);
-            UnvalidatedMessageParameters.ToList().RemoveAll(e => true);
-            AlertGroups.ToList().RemoveAll(e => true);
-            TemplatedRules.ToList().RemoveAll(e => true);
-            RuleTemplates.ToList().RemoveAll(e => true);
-            DefaultNotes.ToList().RemoveAll(e => true);
-            AlertCategoryItems.ToList().RemoveAll(e => true);
-            AlertCategories.ToList().RemoveAll(e => true);
-            SaveChanges();
+            
+            /*RuleCategories.RemoveRange(RuleCategories.ToList());
+            AlertCategoryItems.RemoveRange(AlertCategoryItems.ToList());
+            AlertCategories.RemoveRange(AlertCategories.ToList());
+            Alerts.RemoveRange(Alerts.ToList());
+            Messages.RemoveRange(Messages.ToList());
+            MessageTypes.RemoveRange(MessageTypes.ToList());
+            EscalationChains.RemoveRange(EscalationChains.ToList());
+            EscalationChainLinks.RemoveRange(EscalationChainLinks.ToList());
+            Notifyees.RemoveRange(Notifyees.ToList());
+            NotifyeeGroups.RemoveRange(NotifyeeGroups.ToList());
+            Rules.RemoveRange(Rules.ToList());
+            AlertTypes.RemoveRange(AlertTypes.ToList());
+            RuleCategories.RemoveRange(RuleCategories.ToList());
+            MessageTypeParameterTypes.RemoveRange(MessageTypeParameterTypes.ToList());
+            MessageParameters.RemoveRange(MessageParameters.ToList());
+            AlertParameters.RemoveRange(AlertParameters.ToList());
+            SupportCategories.RemoveRange(SupportCategories.ToList());
+            Engines.RemoveRange(Engines.ToList());
+            AlertStatuses.RemoveRange(AlertStatuses.ToList());
+            UnvalidatedMessages.RemoveRange(UnvalidatedMessages.ToList());
+            UnvalidatedMessageParameters.RemoveRange(UnvalidatedMessageParameters.ToList());
+            AlertGroups.RemoveRange(AlertGroups.ToList());
+            TemplatedRules.RemoveRange(TemplatedRules.ToList());
+            RuleTemplates.RemoveRange(RuleTemplates.ToList());
+            DefaultNotes.RemoveRange(DefaultNotes.ToList());
+
+            SaveChanges();*/
+            ClearDatabase();
         }
     }
 }
